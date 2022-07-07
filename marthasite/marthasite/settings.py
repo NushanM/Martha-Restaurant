9e701c5ab28b9708ba03475c8d9c803173464fb4
Nushan <nushmuthuarachchi@gmail.com>
2022-07-07 14:39:32 +0530

    Bootstrap added

--- marthasite/marthasite/settings.py
+++ marthasite/marthasite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ordering.apps.OrderingConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
